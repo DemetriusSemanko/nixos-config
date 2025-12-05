38f119b02e8d8e178782926853ae1b0e615565a3
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-12-05 12:00:37 -0500

    Added home.nix

--- flake.nix
+++ flake.nix
@@ -4,12 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixvim }: {
+  outputs = { self, nixpkgs, nixvim, home-manager }: {
     nixosConfigurations.delphi = nixpkgs.lib.nixosSystem {
 	modules = [
 	    ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+            	home-manager.useGlobalPkgs = true;
+            	home-manager.useUserPackages = true;
+            	home-manager.users.demsem = ./home.nix;
+
+            	# Optionally, use home-manager.extraSpecialArgs to pass
+            	# arguments to home.nix
+            }
 	];
     };
   };
