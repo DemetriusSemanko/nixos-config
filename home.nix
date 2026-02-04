b4d0591bf38c5f050ff413a5c027fe5e9deef07d
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2026-02-04 10:26:18 -0500

    Added lots of stuff!

--- home.nix
+++ home.nix
@@ -1,40 +1,54 @@
-{inputs, pkgs, ...}: {
-    imports = [
-	inputs.nixvim.homeModules.nixvim
-    ];
+{ inputs, pkgs, ... }:
+{
+  imports = [
+    inputs.nixvim.homeModules.nixvim
+  ];
 
-    home.stateVersion = "26.05";
-    programs.yazi = {
-    	enable = true;
-	enableBashIntegration = true;
+  home.stateVersion = "26.05";
+
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.bash.enable = true;
+
+  programs.nixvim = {
+    enable = true;
+    colorschemes.catppuccin.enable = true;
+    plugins.lspconfig.enable = true;
+    plugins.treesitter = {
+      enable = true;
+      settings = {
+        highlight.enable = true;
+      };
     };
-    programs.bash.enable = true;
-    programs.nixvim = {
-    	enable = true;
-	colorschemes.catppuccin.enable = true;
-	plugins.lspconfig.enable = true;
-    	plugins.treesitter = {
-	    enable = true;
-	    settings = {
-	        highlight.enable = true;
-	    };
-	};
+    lsp = {
+      servers.ruff.enable = true;
+      servers.rust_analyzer.enable = true;
+    };
+    plugins.lspsaga.enable = true;
+    plugins.web-devicons.enable = true;
+    plugins.dropbar.enable = true;
+    plugins.guess-indent.enable = true;
+    plugins.conform-nvim = {
+      enable = true;
+      autoInstall.enable = true;
+      settings.formatters_by_ft = {
+        nix = [ "nixfmt" ];
+        python = [ "ruff_format" ];
+        rust = [ "rustfmt" ];
+      };
+    };
+  };
 
-	lsp = {
-	    servers.ruff.enable = true;
-	    servers.rust_analyzer.enable = true;
-	};
-	plugins.lspsaga.enable = true;
-	plugins.web-devicons.enable = true;
-	plugins.dropbar.enable = true;
-	plugins.guess-indent.enable = true;
-    };   
-    programs.gcc.enable = true;
-    
-    home.packages = [
-    	pkgs.nix-inspect
-	pkgs.nerd-fonts.fira-code
-	pkgs.ghc
-	pkgs.haskellPackages.QuickCheck
-    ];
+  programs.gcc.enable = true;
+  programs.java.enable = true;
+
+  home.packages = [
+    pkgs.nix-inspect
+    pkgs.nerd-fonts.fira-code
+    pkgs.ghc
+    pkgs.haskellPackages.QuickCheck
+  ];
 }
