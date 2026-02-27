bd6a697f4462a79c53c61e0be4f20d2db875d835
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2026-02-27 09:55:16 -0500

    Added wireshark

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,8 @@
     dirb
   ];
 
+  programs.wireshark.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
