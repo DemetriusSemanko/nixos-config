344ce5d0495c1bf9d68c2732153b8834b164713b
Demetrius Semanko <demetrius@demsem.dev>
2026-08-24 15:03:25 -0400

    signing: change behavior from drop to own

--- homeManagerModules/vcs/jj.nix
+++ homeManagerModules/vcs/jj.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       signing = {
-        behavior = "drop";
+        behavior = "own";
         backend = "ssh";
         key = "~/.ssh/id_ed25519.pub";
       };
